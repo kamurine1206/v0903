bf81051e7eb9049a243b0b3f3a76715e34716d30
yometo <blackymt@vip.qq.com>
2020-09-03 11:15:34 +0900

    ランダムに動くラベルたち

--- v0903/Form1.cs
+++ v0903/Form1.cs
@@ -14,6 +14,9 @@ namespace v0903
     {
         int vx = rand.Next (-20,20);
         int vy = rand.Next (-20,20);
+        int vx2 = rand.Next(-20, 20);
+        int vy2 = rand.Next(-20, 20);
+
         int a = Math.Abs(-10);
 
         static Random rand = new Random();
@@ -26,18 +29,22 @@ namespace v0903
             label1.Left = rand.Next(ClientSize.Width - label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
 
+            label2.Left = rand.Next(ClientSize.Width - label2.Width);
+            label2.Top = rand.Next(ClientSize.Height - label2.Height);
+
+
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            vx = 0;
-            vy = 0;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Left += vx;
             label1.Top += vy;
+            label2.Left += vx2;
+            label2.Top += vy2;
             Point mp = MousePosition;
             mp = PointToClient(mp);
 
@@ -54,7 +61,7 @@ namespace v0903
             {
                 vx = -Math.Abs(vx) ;
             }
-            if (label1.Top >= ClientSize.Height)
+            if (label1.Bottom >= ClientSize.Height)
             {
                 vy = -Math.Abs(vy) ;
             }
@@ -62,7 +69,32 @@ namespace v0903
             {
                 timer1.Enabled = false;
             }
+            if (label2.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if (label2.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if (label2.Right >= ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label2.Bottom >= ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+            if ((mp.X >= label1.Left) && (mp.X < label1.Right) && (mp.Y >= label1.Top) && (mp.Y < label1.Bottom))
+            {
+                timer1.Enabled = false;
+            }
 
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
         }
     }
 }
